7b090a18dd43fc75fe6399bf7ec00217bc89334b
ZaneC04 <zenithzsc@gmail.com>
2026-03-16 14:52:10 -0400

    wrote serve single and all todos, update and create todo

--- server/index.js
+++ server/index.js
@@ -42,22 +42,73 @@ const todos = [
 // Response: 200, array of all todos
 
 
+const serveTodos = (req, res, next) => {
+  res.status(200).send(todos)
+}
+
+app.get("/api/todos", serveTodos)
+
 // TODO: GET /api/todos/:id
 // Response: 200, single todo object
 // Error: 404 if no todo with that id
 
+const serveSingleTodo = (req, res, next) => {
+  const { id } = req.params
+  const todo = todos.find(todo => todo.id === Number(id))
+  if (!todo) {
+    res.status(404).send({Error: `no todo with the id ${id}`})
+    return;
+  }
+  res.status(200).send(todo)
+}
+
+app.get("/api/todos/:id", serveSingleTodo)
 
 // TODO: POST /api/todos
 // Request body: { task }
 // Response: 201, the newly created todo object
 // Error: 400 if task is missing from the request body
 
+const createTodo = (req, res, next) => {
+  const { task } = req.body.task
+  if (!task) {
+    res.status(400).send({Error: "Invalid task description"})
+    return;
+  }
+  const newTask = {id: getId(), task: task, isDone: false}
+  res.status(201).send(newTask);
+}
+
+app.post('/api/todos', createTodo)
 
 // TODO: PATCH /api/todos/:id
 // Request body: { isDone }
 // Response: 200, the updated todo object
 // Error: 404 if no todo with that id
 
+const updateTodo = (req, res, next) => {
+  const id = req.params
+  const isDone = req.body.isDone
+  const todo = todos.find(todo => todo.id === Number(id))
+  if (!todo) {
+    res.status(404).send({Error: 'task not found'})
+    return;
+  }
+  todo.isDone = isDone
+  res.status(200).send(todo)
+  // const isDone = req.body.isDone;
+  // const id = req.params.id;
+  // const todo = todos.find((todo) => todo.id === Number(id));
+  // if (!todo) {
+  //   res.status(404).send(`ID not found`);
+  //   return;
+  // }
+  
+  // todo.isDone = isDone;
+  // res.send(todo);
+}
+
+app.patch("/api/todos/:id", updateTodo)
 
 // TODO: DELETE /api/todos/:id
 // Response: 204, no content
